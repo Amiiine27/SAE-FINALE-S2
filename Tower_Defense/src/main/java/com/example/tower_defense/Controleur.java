32fdcfc9dfa0eda537c8192c1a36ca65ce6cc63a
Amiiine27 <amineakh213@outlook.fr>
2023-05-11 16:22:09 +0200

    vue s'affiche

--- Tower_Defense/src/main/java/com/example/tower_defense/Controleur.java
+++ Tower_Defense/src/main/java/com/example/tower_defense/Controleur.java
@@ -1,7 +1,10 @@
 package com.example.tower_defense;
 
 import com.example.tower_defense.modele.Map;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.TilePane;
 
 import java.net.URL;
@@ -9,14 +12,16 @@ import java.util.ResourceBundle;
 
 public class Controleur implements Initializable {
     private Map terrain;
+    @FXML
     private TilePane tilepane;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         terrain = new Map();
+        //remplissage();
     }
 
-    public void remplissage(){
-    }
+
+
 }
--- Tower_Defense/src/main/java/com/example/tower_defense/modele/Map.java
+++ Tower_Defense/src/main/java/com/example/tower_defense/modele/Map.java
@@ -1,16 +1,29 @@
 package com.example.tower_defense.modele;
 
 public class Map {
-    private int[][] map;
+    private int[][] quadrillage;
     private int nbPiratesArrive;
 
     public Map() {
-        map = new int[][]{
+        quadrillage = new int[][]{
                 {0, 1, 0, 0, 0},
                 {0, 1, 0, 0, 0},
                 {0, 1, 1, 1, 1},
                 {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0}
             };
         nbPiratesArrive = 0;
+    }
+
+    public int getYmax(){
+        return this.quadrillage.length;
+    }
+
+    public int getXmax(){
+        return this.quadrillage[0].length;
+    }
+
+    public int valeurDeLaCase(int i, int j){
+        return this.quadrillage[i][j];
     }
 }
