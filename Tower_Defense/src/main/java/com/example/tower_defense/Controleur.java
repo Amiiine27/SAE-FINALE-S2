02e78d726a0d25b42cf49ffec2bf535061b7f641
Amiiine27 <amineakh213@outlook.fr>
2023-05-12 11:26:33 +0200

    Map v1 apparait bien ^^

--- Tower_Defense/src/main/java/com/example/tower_defense/Controleur.java
+++ Tower_Defense/src/main/java/com/example/tower_defense/Controleur.java
@@ -31,31 +31,73 @@ public class Controleur implements Initializable {
                 URL herbeLimiteDroite = getClass().getResource("herbeLimiteDroite.png");
                 URL herbeLimiteBas = getClass().getResource("herbeLimiteBas.png");
                 URL herbeLimiteGauche = getClass().getResource("herbeLimiteGauche.png");
+                URL herbeCoinHautDroite = getClass().getResource("herbeCoinHautDroite.png");
+                URL herbeCoinHautGauche = getClass().getResource("herbeCoinHautGauche.png");
+                URL herbeCoinBasDroite = getClass().getResource("herbeCoinBasDroite.png");
+                URL herbeCoinBasGauche = getClass().getResource("herbeCoinBasGauche.png");
+                URL herbeAngleHautDroite = getClass().getResource("herbeAngleHautDroite.png");
+                URL herbeAngleHautGauche = getClass().getResource("herbeAngleHautGauche.png");
+                URL herbeAngleBasDroite = getClass().getResource("herbeAngleBasDroite.png");
+                URL herbeAngleBasGauche = getClass().getResource("herbeAngleBasGauche.png");
                 URL chemin = getClass().getResource("chemin.png");
                 Image imgHerbeVierge = new Image(String.valueOf(herbeVierge));
                 Image imgChemin = new Image(String.valueOf(chemin));
-                Image imgherbeLimiteHaut = new Image(String.valueOf(herbeLimiteHaut));
-                Image imgherbeLimiteDroite = new Image(String.valueOf(herbeLimiteDroite));
-                Image imgherbeLimiteBas = new Image(String.valueOf(herbeLimiteBas));
-                Image imgherbeLimiteGauche = new Image(String.valueOf(herbeLimiteGauche));
-                if (n == 0){
+                Image imgHerbeLimiteHaut = new Image(String.valueOf(herbeLimiteHaut));
+                Image imgHerbeLimiteDroite = new Image(String.valueOf(herbeLimiteDroite));
+                Image imgHerbeLimiteBas = new Image(String.valueOf(herbeLimiteBas));
+                Image imgHerbeLimiteGauche = new Image(String.valueOf(herbeLimiteGauche));
+                Image imgHerbeCoinHautDroite = new Image(String.valueOf(herbeCoinHautDroite));
+                Image imgHerbeCoinHautGauche = new Image(String.valueOf(herbeCoinHautGauche));
+                Image imgHerbeCoinBasDroite = new Image(String.valueOf(herbeCoinBasDroite));
+                Image imgHerbeCoinBasGauche = new Image(String.valueOf(herbeCoinBasGauche));
+                Image imgHerbeAngleBasGauche = new Image(String.valueOf(herbeAngleBasGauche));
+                Image imgHerbeAngleHautDroite = new Image(String.valueOf(herbeAngleHautDroite));
+                Image imgHerbeAngleHautGauche = new Image(String.valueOf(herbeAngleHautGauche));
+                Image imgHerbeAngleBasDroite = new Image(String.valueOf(herbeAngleBasDroite));
+
+                if (n == 0) {
                     myImageView.setImage(imgHerbeVierge);
                 }
-                else if (n == 1){
+                else if (n == 1) {
                     myImageView.setImage(imgChemin);
                 }
-                else if (n == 2){
-                    myImageView.setImage(imgherbeLimiteHaut);
+                else if (n == 2) {
+                    myImageView.setImage(imgHerbeLimiteHaut);
                 }
-                else if (n == 3){
-                    myImageView.setImage(imgherbeLimiteDroite);
+                else if (n == 3) {
+                    myImageView.setImage(imgHerbeLimiteDroite);
                 }
-                else if (n == 4){
-                    myImageView.setImage(imgherbeLimiteBas);
+                else if (n == 4) {
+                    myImageView.setImage(imgHerbeLimiteBas);
                 }
-                else if (n == 5){
-                    myImageView.setImage(imgherbeLimiteGauche);
+                else if (n == 5) {
+                    myImageView.setImage(imgHerbeLimiteGauche);
                 }
+                else if (n == 6) {
+                    myImageView.setImage(imgHerbeCoinHautDroite);
+                }
+                else if (n == 7) {
+                    myImageView.setImage(imgHerbeCoinHautGauche);
+                }
+                else if (n == 8) {
+                    myImageView.setImage(imgHerbeCoinBasDroite);
+                }
+                else if (n == 9) {
+                    myImageView.setImage(imgHerbeCoinBasGauche);
+                }
+                else if (n == 10) {
+                    myImageView.setImage(imgHerbeAngleBasGauche);
+                }
+                else if (n == 11) {
+                    myImageView.setImage(imgHerbeAngleHautDroite);
+                }
+                else if (n == 12) {
+                    myImageView.setImage(imgHerbeAngleHautGauche);
+                }
+                else if (n == 13) {
+                    myImageView.setImage(imgHerbeAngleBasDroite);
+                }
+
                 tilepane.getChildren().add(myImageView);
             }
         }
--- Tower_Defense/src/main/java/com/example/tower_defense/Main.java
+++ Tower_Defense/src/main/java/com/example/tower_defense/Main.java
@@ -12,7 +12,7 @@ import static javafx.application.Application.launch;
 public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("vue.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 180, 180);
+        Scene scene = new Scene(fxmlLoader.load(), 220, 260);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setResizable(false); // ne plus resizer la fenêtre
