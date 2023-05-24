5c588f3a2597c65e9c5827eaabb0a2b8a4591535
Ayaube <122544136+Ayaube@users.noreply.github.com>
2023-05-24 11:21:29 +0200

    Delete tower_defense directory

--- tower_defense/modele/Shichibukais.java
+++
@@ -1,8 +0,0 @@
-package com.example.tower_defense.modele;
-
-public class Shichibukais extends Soldat {
-
-    public Shichibukais(double x0, double y0) {
-        super(x0, y0, 10000, 300, 20, true);              // Ennemi True car ce sera un Soldat exclusivement Ennemi
-    }
-}
--- tower_defense/modele/SuperNova.java
+++
@@ -1,8 +0,0 @@
-package com.example.tower_defense.modele;
-
-public class SuperNova extends Soldat {
-
-    public SuperNova(double x0, double y0) {
-        super(x0, y0, 5000, 130, 12, true);              // Ennemi True car ce sera un Soldat exclusivement Ennemi
-    }
-}
