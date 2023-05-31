66f18239b53fb01e7690b1a746878ba41fc3c46c
Amiiine27 <amineakh213@outlook.fr>
2023-05-31 09:33:43 +0200

    Code Stable. ajout des tours dans le bottom

--- src/main/java/fr/iut/montreuil/Red_Line_Defense/ControleurMP.java
+++ src/main/java/fr/iut/montreuil/Red_Line_Defense/ControleurMP.java
@@ -17,6 +17,7 @@ import java.util.ResourceBundle;
 public class ControleurMP implements Initializable {
     private Stage stage;
     private Parent root;
+    private Font jap;
 
 
     @FXML
@@ -24,8 +25,8 @@ public class ControleurMP implements Initializable {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fr/iut/montreuil/Red_Line_Defense/Vues/vueDeJeu.fxml"));
         root = loader.load();
         stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root, 840, 480);
-        stage.setResizable(false);
+        Scene scene = new Scene(root, 940, 560); // Largeur 940px : 840px pour la carte, 100px pour le volet droit
+        stage.setResizable(false);                     // Hauteur 560px : 480 pour la carte, 80px pour le volet bas
         stage.setTitle("Red Line Defense");
         stage.setScene(scene);
         stage.show();
