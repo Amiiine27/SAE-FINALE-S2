78023327d83fa2a522f03505e3e19463f95754ee
Ayaube <ayoub.lamchichi.info@gmail.com>
2023-06-01 11:00:00 +0200

    elagage du code

--- src/main/java/fr/iut/montreuil/Red_Line_Defense/Controleur.java
+++ src/main/java/fr/iut/montreuil/Red_Line_Defense/Controleur.java
@@ -66,33 +66,10 @@ public class Controleur implements Initializable {
                 int n = terrain.valeurDeLaCase(i, j);
                 ImageView myImageView = new ImageView();
                 URL herbeVierge = getClass().getResource("Images/herbeVierge.png");
-                /* URL herbeLimiteHaut = getClass().getResource("herbeLimiteHaut.png");
-                URL herbeLimiteDroite = getClass().getResource("herbeLimiteDroite.png");
-                URL herbeLimiteBas = getClass().getResource("herbeLimiteBas.png");
-                URL herbeLimiteGauche = getClass().getResource("herbeLimiteGauche.png");
-                URL herbeCoinHautDroite = getClass().getResource("herbeCoinHautDroite.png");
-                URL herbeCoinHautGauche = getClass().getResource("herbeCoinHautGauche.png");
-                URL herbeCoinBasDroite = getClass().getResource("herbeCoinBasDroite.png");
-                URL herbeCoinBasGauche = getClass().getResource("herbeCoinBasGauche.png");
-                URL herbeAngleHautDroite = getClass().getResource("herbeAngleHautDroite.png");
-                URL herbeAngleHautGauche = getClass().getResource("herbeAngleHautGauche.png");
-                URL herbeAngleBasDroite = getClass().getResource("herbeAngleBasDroite.png");
-                URL herbeAngleBasGauche = getClass().getResource("herbeAngleBasGauche.png"); */
                 URL chemin = getClass().getResource("Images/chemin.png");
                 Image imgHerbeVierge = new Image(String.valueOf(herbeVierge));
                 Image imgChemin = new Image(String.valueOf(chemin));
-                /* Image imgHerbeLimiteHaut = new Image(String.valueOf(herbeLimiteHaut));
-                Image imgHerbeLimiteDroite = new Image(String.valueOf(herbeLimiteDroite));
-                Image imgHerbeLimiteBas = new Image(String.valueOf(herbeLimiteBas));
-                Image imgHerbeLimiteGauche = new Image(String.valueOf(herbeLimiteGauche));
-                Image imgHerbeCoinHautDroite = new Image(String.valueOf(herbeCoinHautDroite));
-                Image imgHerbeCoinHautGauche = new Image(String.valueOf(herbeCoinHautGauche));
-                Image imgHerbeCoinBasDroite = new Image(String.valueOf(herbeCoinBasDroite));
-                Image imgHerbeCoinBasGauche = new Image(String.valueOf(herbeCoinBasGauche));
-                Image imgHerbeAngleBasGauche = new Image(String.valueOf(herbeAngleBasGauche));
-                Image imgHerbeAngleHautDroite = new Image(String.valueOf(herbeAngleHautDroite));
-                Image imgHerbeAngleHautGauche = new Image(String.valueOf(herbeAngleHautGauche));
-                Image imgHerbeAngleBasDroite = new Image(String.valueOf(herbeAngleBasDroite));*/
+
 
                 myImageView.setTranslateX(j * tailleImage);
                 myImageView.setTranslateY(i * tailleImage);
@@ -105,42 +82,7 @@ public class Controleur implements Initializable {
                 else if (n == 1) {
                     myImageView.setImage(imgChemin);
                 }
-                /* else if (n == 2) {
-                    myImageView.setImage(imgHerbeLimiteHaut);
-                }
-                else if (n == 3) {
-                    myImageView.setImage(imgHerbeLimiteDroite);
-                }
-                else if (n == 4) {
-                    myImageView.setImage(imgHerbeLimiteBas);
-                }
-                else if (n == 5) {
-                    myImageView.setImage(imgHerbeLimiteGauche);
-                }
-                else if (n == 6) {
-                    myImageView.setImage(imgHerbeCoinHautDroite);
-                }
-                else if (n == 7) {
-                    myImageView.setImage(imgHerbeCoinHautGauche);
-                }
-                else if (n == 8) {
-                    myImageView.setImage(imgHerbeCoinBasDroite);
-                }
-                else if (n == 9) {
-                    myImageView.setImage(imgHerbeCoinBasGauche);
-                }
-                else if (n == 10) {
-                    myImageView.setImage(imgHerbeAngleBasGauche);
-                }
-                else if (n == 11) {
-                    myImageView.setImage(imgHerbeAngleHautDroite);
-                }
-                else if (n == 12) {
-                    myImageView.setImage(imgHerbeAngleHautGauche);
-                }
-                else if (n == 13) {
-                    myImageView.setImage(imgHerbeAngleBasDroite);
-                } */
+
 
                 centerPane.getChildren().add(myImageView);
             }
@@ -161,10 +103,10 @@ public class Controleur implements Initializable {
         int[] possibleDestX = {88, 89, 90};
 
         int startX = possibleStartX[rand.nextInt(possibleStartX.length)];
-        int startY = 59;  // Supposons que cette valeur est constante.
+        int startY = 59;  // cette valeur est constante.
 
         int destX = possibleDestX[rand.nextInt(possibleDestX.length)];
-        int destY = 47;  // Supposons que cette valeur est constante.
+        int destY = 47;  // cette valeur est constante.
 
         Rookie rookie = new Rookie(startX * tailleImage, startY * tailleImage, destX * tailleImage, destY * tailleImage);
         List<Point2D> chemin = gestionnaireDeDeplacement.deplacerRookie(startX, startY, destX, destY);
