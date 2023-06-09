7d78cda1ee101e060e5f8fc1924572640fd8c38a
Rayankz <kherouarayan@gmail.com>
2023-06-09 11:55:37 +0200

    Modifications

--- src/main/java/fr/iut/montreuil/Red_Line_Defense/modele/VuesModele/VueProjectile.java
+++ src/main/java/fr/iut/montreuil/Red_Line_Defense/modele/VuesModele/VueProjectile.java
@@ -1,4 +1,14 @@
 package fr.iut.montreuil.Red_Line_Defense.modele.VuesModele;
 
+import fr.iut.montreuil.Red_Line_Defense.modele.Carte;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
 public class VueProjectile {
+    private Carte terrain;
+
+    @FXML
+    private Pane centerPane;
+
+
 }
